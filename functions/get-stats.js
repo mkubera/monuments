a3d5be5aff2ec1f9d898d328796a9eb3862a0432
mkubera <rainteller@tutanota.de>
2021-02-15 18:02:06 +0100

    First test + test of netlify fn

--- functions/get-stats.js
+++ functions/get-stats.js
@@ -1,11 +1,5 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || "World";
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}!`,
-  };
-
   var Airtable = require("airtable");
   var base = new Airtable({
     apiKey: "YOUR_API_KEY",
@@ -24,6 +18,11 @@ exports.handler = async (event) => {
         records.forEach(function (record) {
           console.log("Retrieved", record.get("Name"));
         });
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(records),
+        };
 
         // To fetch the next page of records, call `fetchNextPage`.
         // If there are more records, `page` will get called again.
--- tests/Example.elm
+++ tests/Example.elm
@@ -3,8 +3,26 @@ module Example exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
-
+import Update.Attention exposing (..)
+import Model exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    -- todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "Update.Attention module" [
+      describe "Update.Attention.newBuildings" [
+        test "Creates a new building" <|
+        \_ ->
+          let
+            (Faction _ fBuildings _) = initP1
+            newBuildingType = ThirdEyeCleansers
+            newBuilding = (Building newBuildingType NoAttention Low)
+            expectedBuildings = [ newBuilding
+                , Building (MonumentOfUs 1) NoAttention High
+                , Building PsycheDancers NoAttention Low
+                ]
+          in
+            newBuildings fBuildings [] newBuildingType
+            |> Expect.equal expectedBuildings
+      ]
+    ]
