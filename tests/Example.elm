68508e56cd7dff70ae768daae798ea84fe3468ce
mkubera <rainteller@tutanota.de>
2021-03-09 17:45:16 +0100

    Added new test (delete it later)

--- tests/Example.elm
+++ tests/Example.elm
@@ -24,5 +24,9 @@ suite =
           in
             newBuildings fBuildings [] newBuildingType
             |> Expect.equal expectedBuildings
+
+        ,test "Test test" <|
+        \_ ->
+          Expect.equal 1 1
       ]
     ]
